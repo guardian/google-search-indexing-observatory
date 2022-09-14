4ae4480ad06ecf45bfc6e5c0b9d39b4126d29615
Dina Hafez <dina.hafez@guardian.co.uk>
2022-09-14 14:31:49 +0100

    Add code to read CAPI client key
    Co-Authored-By: Roberto Tyley <52038+rtyley@users.noreply.github.com>

--- src/main/scala/ophan/google/index/checker/Lambda.scala
+++ src/main/scala/ophan/google/index/checker/Lambda.scala
@@ -3,6 +3,7 @@ package ophan.google.index.checker
 import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent
 import com.gu.contentapi.client.GuardianContentClient
+import com.gu.contentapi.client.model.SearchQuery
 import ophan.google.index.checker.logging.Logging
 import software.amazon.awssdk.services.s3.model.PutObjectRequest
 
@@ -15,7 +16,13 @@ object Lambda extends Logging {
    * Logic handler
    */
   def go(): Unit = {
-    val client = new GuardianContentClient("your-api-key")
+    val capiKey: String = AWS.SSM.getParameter(_.withDecryption(true).name("/Ophan/dashboard-es7/CODE/ContentApiKey")).parameter.value
+    val client = new GuardianContentClient(capiKey)
+
+    val allContentSearch = SearchQuery().orderBy()
+    client.getResponse(allContentSearch) map { response =>
+      println(response.total)
+    }
   }
 
   // See https://docs.aws.amazon.com/AmazonS3/latest/userguide/finding-canonical-user-id.html
