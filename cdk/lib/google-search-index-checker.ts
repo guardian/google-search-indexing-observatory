d96e79d355a5d80424dfdaa700e792ea79550c47
Roberto Tyley <roberto.tyley@gmail.com>
2022-09-20 18:07:50 +0100

    Some CDK stuff that doesn't quite seem to work...

--- cdk/lib/google-search-index-checker.ts
+++ cdk/lib/google-search-index-checker.ts
@@ -8,18 +8,42 @@ import {Runtime} from "aws-cdk-lib/aws-lambda";
 import {Schedule} from "aws-cdk-lib/aws-events";
 import {Duration} from "aws-cdk-lib";
 import {NoMonitoring} from "@guardian/cdk/lib/constructs/cloudwatch";
+import {Table} from "aws-cdk-lib/aws-dynamodb";
+import {AttributeType} from "aws-cdk-lib/aws-dynamodb/lib/table";
+import {
+  Effect,
+  OrganizationPrincipal,
+  PolicyStatement,
+} from "@aws-cdk/aws-iam";
 
 export class GoogleSearchIndexChecker extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
     const noMonitoring: NoMonitoring = { noMonitoring: true };
-    new GuScheduledLambda(this, "scheduledLambda", {
+    const lambda = new GuScheduledLambda(this, "scheduledLambda", {
       app: "google-search-index-checker",
       fileName: "google-search-index-checker.jar",
       handler: "ophan.google.index.checker.Lambda::handler",
       monitoringConfiguration: noMonitoring,
-      rules: [{ schedule: Schedule.rate(Duration.minutes(1)) }],
+      rules: [{ schedule: Schedule.rate(Duration.minutes(2)) }],
       runtime: Runtime.JAVA_11
     })
+
+    const table = new Table(this, "AvailabilityTable", {
+      partitionKey: {
+        name: "capiId",
+        type: AttributeType.STRING
+      },
+      tableName: `google-search-index-availability`
+    });
+
+    lambda.addToRolePolicy(
+        new PolicyStatement({
+          effect: Effect.ALLOW,
+          resources: [table.tableArn],
+          actions: ["dynamodb:BatchGetItem", "dynamodb:UpdateItem"]
+        })
+    );
+
   }
 }
