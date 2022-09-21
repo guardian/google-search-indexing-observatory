d950765dd7030acfeaeda1cc11cc17f976e44344
Dina Hafez <dina.hafez@guardian.co.uk>
2022-09-21 14:43:23 +0100

    Added dynamo db table and configured CI
    Co-Authored-By: Roberto Tyley <52038+rtyley@users.noreply.github.com>
    Co-Authored-By: Akash Askoolum <akash1810@users.noreply.github.com>

--- build.sbt
+++ build.sbt
@@ -38,7 +38,7 @@ assemblyJarName := s"${name.value}.jar"
 riffRaffPackageType := assembly.value
 riffRaffUploadArtifactBucket := Option("riffraff-artifact")
 riffRaffUploadManifestBucket := Option("riffraff-builds")
-riffRaffArtifactResources += (file("cfn.yaml"), s"cloudformation/cfn.yaml")
+riffRaffArtifactResources += (file("cdk/cdk.out/GoogleSearchIndexChecker-PROD.template.json"), s"cloudformation/GoogleSearchIndexChecker-PROD.template.json")
 
 assembly / assemblyMergeStrategy := {
   case PathList("META-INF", "MANIFEST.MF") => MergeStrategy.discard
--- cdk/lib/google-search-index-checker.ts
+++ cdk/lib/google-search-index-checker.ts
@@ -1,49 +1,31 @@
-import { join } from "path";
 import type { GuStackProps } from "@guardian/cdk/lib/constructs/core";
 import { GuStack } from "@guardian/cdk/lib/constructs/core";
 import type { App } from "aws-cdk-lib";
-import { CfnInclude } from "aws-cdk-lib/cloudformation-include";
 import {GuScheduledLambda} from "@guardian/cdk";
 import {Runtime} from "aws-cdk-lib/aws-lambda";
 import {Schedule} from "aws-cdk-lib/aws-events";
 import {Duration} from "aws-cdk-lib";
 import {NoMonitoring} from "@guardian/cdk/lib/constructs/cloudwatch";
-import {Table} from "aws-cdk-lib/aws-dynamodb";
-import {AttributeType} from "aws-cdk-lib/aws-dynamodb/lib/table";
-import {
-  Effect,
-  OrganizationPrincipal,
-  PolicyStatement,
-} from "@aws-cdk/aws-iam";
+import {AttributeType, Table} from "aws-cdk-lib/aws-dynamodb";
 
 export class GoogleSearchIndexChecker extends GuStack {
   constructor(scope: App, id: string, props: GuStackProps) {
     super(scope, id, props);
     const noMonitoring: NoMonitoring = { noMonitoring: true };
-    const lambda = new GuScheduledLambda(this, "scheduledLambda", {
+    const scheduledLambda = new GuScheduledLambda(this, "scheduledLambda", {
       app: "google-search-index-checker",
       fileName: "google-search-index-checker.jar",
       handler: "ophan.google.index.checker.Lambda::handler",
       monitoringConfiguration: noMonitoring,
-      rules: [{ schedule: Schedule.rate(Duration.minutes(2)) }],
+      rules: [{ schedule: Schedule.rate(Duration.minutes(1)) }],
       runtime: Runtime.JAVA_11
     })
-
-    const table = new Table(this, "AvailabilityTable", {
+    const table = new Table(this, 'Table', {
       partitionKey: {
-        name: "capiId",
-        type: AttributeType.STRING
+        name: 'capiId',
+        type: AttributeType.STRING,
       },
-      tableName: `google-search-index-availability`
     });
-
-    lambda.addToRolePolicy(
-        new PolicyStatement({
-          effect: Effect.ALLOW,
-          resources: [table.tableArn],
-          actions: ["dynamodb:BatchGetItem", "dynamodb:UpdateItem"]
-        })
-    );
-
+    table.grantReadWriteData(scheduledLambda)
   }
 }
