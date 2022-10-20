f47e175365d2c9801f305c8370d58fcd5c284999
Olly <9575458+OllysCoding@users.noreply.github.com>
2022-10-20 16:25:35 +0100

    Add BBC & Daily mail to Site.scala

--- src/main/scala/ophan/google/indexing/observatory/model/Site.scala
+++ src/main/scala/ophan/google/indexing/observatory/model/Site.scala
@@ -11,14 +11,35 @@ object Sites {
     sitemaps=Set(new URI("https://www.nytimes.com/sitemaps/new/news-1.xml.gz"))
   )
 
-  val Independent= Site(
-    url = "https://www.independent.co.uk/",
-    searchEngineId = "119a788f858c94b0f",
-    sitemaps = Set(new URI("https://www.independent.co.uk/sitemaps/googlenews"))
+// The Independent appear to change/evolve their URLs often, leaving them in the sitemap with
+// redirects to the new URL which is often also in the sitemap, this doesn't work very well for us
+// as we don't check if URLs redirect - meaning we think content is missing when its actually just
+// been redirected elsewhere.
+//  val Independent= Site(
+//    url = "https://www.independent.co.uk/",
+//    searchEngineId = "119a788f858c94b0f",
+//    sitemaps = Set(new URI("https://www.independent.co.uk/sitemaps/googlenews"))
+//  )
+
+  val BBC = Site(
+    url = "https://www.bbc.co.uk",
+    searchEngineId = "b44c6a8d0e79b43bb",
+    sitemaps = Set(
+      new URI("https://www.bbc.co.uk/sitemaps/https-sitemap-uk-news-1.xml"),
+      new URI("https://www.bbc.co.uk/sitemaps/https-sitemap-uk-news-2.xml")
+    )
+  )
+
+  val DailyMail= Site(
+    url = "https://www.dailymail.co.uk",
+    searchEngineId = "e42e842597f034061",
+    sitemaps = Set(new URI("https://www.dailymail.co.uk/google-news-sitemap1.xml"))
   )
 
   val All = Set(
-    NewYorkTimes
+    NewYorkTimes,
+    BBC,
+    DailyMail
     //Independent
   )
 }
