a5c879e843dae000172d0a7905937323d1009c7b
Roberto Tyley <roberto.tyley@gmail.com>
2022-11-02 15:51:57 +0000

    Record some additional sites, don't scan them yet

--- src/main/scala/ophan/google/indexing/observatory/model/Site.scala
+++ src/main/scala/ophan/google/indexing/observatory/model/Site.scala
@@ -15,11 +15,11 @@ object Sites {
 // redirects to the new URL which is often also in the sitemap, this doesn't work very well for us
 // as we don't check if URLs redirect - meaning we think content is missing when its actually just
 // been redirected elsewhere.
-//  val Independent= Site(
-//    url = "https://www.independent.co.uk/",
-//    searchEngineId = "119a788f858c94b0f",
-//    sitemaps = Set(new URI("https://www.independent.co.uk/sitemaps/googlenews"))
-//  )
+  val Independent= Site(
+    url = "https://www.independent.co.uk/",
+    searchEngineId = "119a788f858c94b0f",
+    sitemaps = Set(new URI("https://www.independent.co.uk/sitemaps/googlenews"))
+  )
 
   val BBC = Site(
     url = "https://www.bbc.co.uk",
@@ -36,10 +36,27 @@ object Sites {
     sitemaps = Set(new URI("https://www.dailymail.co.uk/google-news-sitemap1.xml"))
   )
 
+  val WashingtonPost = Site(
+    url = "https://www.washingtonpost.com/",
+    searchEngineId = "65742b4f9808f4c04",
+    sitemaps = Set(new URI("https://www.washingtonpost.com/arcio/news-sitemap/"))
+  )
+
+  val Telegraph = Site(
+    url = "https://www.telegraph.co.uk/",
+    searchEngineId = "94687f1b5a1994152",
+    sitemaps = Set(new URI("https://www.telegraph.co.uk/sitemap-news.xml"))
+  )
+
+  val Sun = Site( // google-indexing-observatory-
+    url = "https://www.thesun.co.uk/",
+    searchEngineId = "943ddaf5b540f41a3",
+    sitemaps = Set(new URI("https://www.thesun.co.uk/news-sitemap.xml"))
+  )
+
   val All = Set(
     NewYorkTimes,
     BBC,
     DailyMail
-    //Independent
   )
 }
