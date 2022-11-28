ec32f8a3d1986688c4cd55a6a38186d5997d6c9a
Roberto Tyley <roberto.tyley@gmail.com>
2022-11-28 09:29:44 +0000

    Add redirect-resolving code
    The BBC have share location urls in their sitemap which redirect, eg:
    
    https://www.bbc.co.uk/news/uk-politics-63534039 - in sitemap
    https://www.bbc.co.uk/news/av/uk-politics-63534039 - ultimate url
    
    ...it's only the ultimate url (with '/av', in this case) that can be
    found in Google Search results.
    
    Updated process
    ---------------
    
    Resolution of new urls only occurs when the url is first found in the
    sitemap (we don't want to have to repeatedly resolve the same urls
    over and over again) - this redirected url is stored in the new
    `finalUriAfterRedirects` column.
    
    Urls are never searched for in Google Search when we first see them in
    the sitemap - we wait until we have had a new lambda run, and the url
    has been loaded as an existing `AvailabilityRecord` in dynamodb.
    It's at that point that we search for the redirected url, which we
    recorded earlier.
    
    Which url to use when?
    ----------------------
    
    * original url : needed as the primary key, because the observatory
      lambda uses it to lookup every entry from the sitemap (we don't want
      to have to resolve every url from the sitemap every time we load the sitemap)
    * redirected url : when we search google for the content, we need to be using
      the redirected url when we're checking for matches.
    
    Added logging of bad HTTP-response status codes
    
    As suggested by Sam!

--- src/test/scala/ophan/google/indexing/observatory/model/AvailabilityRecordTest.scala
+++ src/test/scala/ophan/google/indexing/observatory/model/AvailabilityRecordTest.scala
@@ -1,11 +1,15 @@
 package ophan.google.indexing.observatory.model
 
+import ophan.google.indexing.observatory.{RedirectPath, Resolution}
+import ophan.google.indexing.observatory.literals.uri
 import ophan.google.indexing.observatory.model.AvailabilityRecord.Field.FirstSeenInSitemapDateIndexKey
 import ophan.google.indexing.observatory.model.AvailabilityRecord.{DelayForFirstCheckAfterContentIsFirstSeenInSitemap, reasonableTimeBetweenChecksForContentAged}
 import org.scalatest.OptionValues
+import org.scalatest.concurrent.{IntegrationPatience, ScalaFutures}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scanamo.{DynamoObject, DynamoValue}
+import ophan.google.indexing.observatory.Resolution.Resolved
 
 import java.net.URI
 import java.time.{Duration, Instant}
@@ -13,7 +17,7 @@ import java.time.Duration.{ofHours, ofMinutes, ofSeconds}
 import java.time.temporal.ChronoUnit.SECONDS
 import scala.math.Ordering.Implicits.*
 
-class AvailabilityRecordTest extends AnyFlatSpec with Matchers with OptionValues {
+class AvailabilityRecordTest extends AnyFlatSpec with Matchers with OptionValues  with ScalaFutures with IntegrationPatience {
 
   val elapsedTimeAtEachCheck: LazyList[Duration] = LazyList.iterate(DelayForFirstCheckAfterContentIsFirstSeenInSitemap) { accumulatedTime =>
     accumulatedTime.plus(reasonableTimeBetweenChecksForContentAged(accumulatedTime))
@@ -44,16 +48,33 @@ class AvailabilityRecordTest extends AnyFlatSpec with Matchers with OptionValues
 
   it should "serialise to Dynamo" in {
     val ar = AvailabilityRecord(
-      uri = new URI("https://www.theguardian.com/foo"),
+      uri = uri"https://www.theguardian.com/foo",
+      finalUriAfterRedirects = Some(uri"https://www.theguardian.com/bar"),
+      uriResolvedOk = true,
       firstSeenInSitemap = Instant.parse("2022-11-02T16:41:37Z"),
       missing = Some(Instant.parse("2022-12-05T20:10:34Z")),
       found = None
     )
     val obj: DynamoObject = AvailabilityRecord.formatAvailabilityRecord.write(ar).asObject.value
     obj("uri").value shouldBe DynamoValue.fromString("https://www.theguardian.com/foo")
+    obj("finalUriAfterRedirects").value shouldBe DynamoValue.fromString("https://www.theguardian.com/bar")
+    obj("uriResolvedOk").value shouldBe DynamoValue.fromBoolean(true)
     obj("firstSeenInSitemap").value shouldBe DynamoValue.fromString("2022-11-02T16:41:37Z")
     obj("missing").value shouldBe DynamoValue.fromString("2022-12-05T20:10:34Z")
     obj("found") shouldBe None
     obj(FirstSeenInSitemapDateIndexKey).value shouldBe DynamoValue.fromString("2022-11-02Z")
+  }
+
+  it should "Create an AvailabilityRecord instance and the finalUriAfterRedirects property is correct" in {
+    val resolved: Resolution.Resolved = Resolution.Resolved(RedirectPath(Seq(
+      uri"https://www.bbc.co.uk/news/uk-politics-63534039",
+      uri"https://www.bbc.co.uk/news/av/uk-politics-63534039"
+    )), ok = true)
+
+    val availabilityRecord: AvailabilityRecord = AvailabilityRecord(resolved, Instant.parse("2022-12-05T20:10:34Z"))
+
+    availabilityRecord.uri shouldBe uri"https://www.bbc.co.uk/news/uk-politics-63534039"
+    availabilityRecord.finalUriAfterRedirects.value shouldBe uri"https://www.bbc.co.uk/news/av/uk-politics-63534039"
+    availabilityRecord.ultimateUri shouldBe uri"https://www.bbc.co.uk/news/av/uk-politics-63534039"
   }
 }
