7cf7dcb430d7330e8b08fd2a81ba1d3c33052ecf
Roberto Tyley <roberto.tyley@gmail.com>
2022-11-28 09:29:44 +0000

    Add redirect-resolving code
    The BBC have share location urls in their sitemap which redirect, eg:
    
    https://www.bbc.co.uk/news/uk-politics-63534039 - in sitemap
    https://www.bbc.co.uk/news/av/uk-politics-63534039 - ultimate url
    
    ...it's only the ultimate url (with '/av', in this case) that can be
    found in Google Search results.
    
    Updated process
    ---------------
    
    Resolution of new urls only occurs when the url is first found in the
    sitemap (we don't want to have to repeatedly resolve the same urls
    over and over again) - this redirected url is stored in the new
    `finalUriAfterRedirects` column.
    
    Urls are never searched for in Google Search when we first see them in
    the sitemap - we wait until we have had a new lambda run, and the url
    has been loaded as an existing `AvailabilityRecord` in dynamodb.
    It's at that point that we search for the redirected url, which we
    recorded earlier.
    
    Which url to use when?
    ----------------------
    
    * original url : needed as the primary key, because the observatory
      lambda uses it to lookup every entry from the sitemap (we don't want
      to have to resolve every url from the sitemap every time we load the sitemap)
    * redirected url : when we search google for the content, we need to be using
      the redirected url when we're checking for matches.
    
    Added logging of bad HTTP-response status codes
    
    As suggested by Sam!

--- src/main/scala/ophan/google/indexing/observatory/DataStore.scala
+++ src/main/scala/ophan/google/indexing/observatory/DataStore.scala
@@ -1,12 +1,12 @@
 package ophan.google.indexing.observatory
 
-import DataStore.{scanamoAsync, table}
+import ophan.google.indexing.observatory.DataStore.{scanamoAsync, table}
+import ophan.google.indexing.observatory.Resolution.Resolved
 import ophan.google.indexing.observatory.logging.Logging
-import ophan.google.indexing.observatory.model.AvailabilityRecord._
+import ophan.google.indexing.observatory.model.AvailabilityRecord.*
 import ophan.google.indexing.observatory.model.{AvailabilityRecord, CheckReport}
-import org.scanamo._
-import org.scanamo.syntax._
-import org.scanamo.update.UpdateExpression
+import org.scanamo.*
+import org.scanamo.syntax.*
 
 import java.net.URI
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -21,24 +21,29 @@ object DataStore {
 }
 
 case class DataStore() extends Logging {
-  def fetchExistingRecordsFor(uris: Set[URI]): Future[Map[URI,AvailabilityRecord]] = scanamoAsync.exec(
+  def fetchExistingRecordsFor(uris: Set[URI]): Future[Set[AvailabilityRecord]] = scanamoAsync.exec(
     table.getAll(Field.Uri in uris)
-  ).map(_.flatMap(_.toOption).map(record => record.uri -> record).toMap)
+  ).map(_.flatMap(_.toOption))
 
-  def storeNewRecordsFor(sitemapDownload: SitemapDownload, alreadyKnownUris: Set[URI]): Future[Unit] = {
-    val urisNotSeenBefore = sitemapDownload.allUris -- alreadyKnownUris
+
+  /**
+   * When we initially store an availability record in the DynamoDB table, we don't store anything about its
+   * availability, just its URL, whether it redirects, and the time we first have seen this url.
+   */
+  def storeNewRecordsFor(sitemapDownload: SitemapDownload, urisNotSeenBefore: Set[URI]): Future[Unit] = {
     if (urisNotSeenBefore.isEmpty) Future.successful(()) else {
       logger.info(Map(
         "site" -> sitemapDownload.site.url,
         "sitemap.uris.all" -> sitemapDownload.allUris.size,
-        "sitemap.uris.old" -> alreadyKnownUris.size,
-        "sitemap.uris.new" -> urisNotSeenBefore.size
-      ), s"Storing ${urisNotSeenBefore.size} new uris for ${sitemapDownload.site.url}. Sample: ${urisNotSeenBefore.take(2).mkString(",")}")
+      ) ++ contextSampleOf("sitemap.uris.urisNotSeenBefore", urisNotSeenBefore),
+        s"Storing new uris for ${sitemapDownload.site.url}")
       scanamoAsync.exec(
-        table.putAll(urisNotSeenBefore.map(uri => AvailabilityRecord(uri, sitemapDownload.timestamp)))
+        table.putAll(urisNotSeenBefore.map { uri =>
+          val skippedResolution: Resolved = Resolution.Resolved(RedirectPath(Seq(uri)), false)
+          AvailabilityRecord(skippedResolution, sitemapDownload.timestamp)
+        })
       )
     }
-
   }
 
   def update(uri: URI, checkReport: CheckReport): Future[Option[AvailabilityRecord]] = {
