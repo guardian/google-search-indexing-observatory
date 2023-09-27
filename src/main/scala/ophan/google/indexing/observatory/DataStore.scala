3b8cce0e1a470f2f08ee7392ceda8f0db2c36b00
Roberto Tyley <roberto.tyley@gmail.com>
2023-09-27 17:15:09 +0100

    Debug logging - why is that statement not getting logged

--- src/main/scala/ophan/google/indexing/observatory/DataStore.scala
+++ src/main/scala/ophan/google/indexing/observatory/DataStore.scala
@@ -28,13 +28,14 @@ case class DataStore() extends Logging {
   def storeNewRecordsFor(sitemapDownload: SitemapDownload, alreadyKnownUris: Set[URI]): Future[Unit] = {
     val urisNotSeenBefore = sitemapDownload.allUris -- alreadyKnownUris
     println(s"urisNotSeenBefore=$urisNotSeenBefore site=${sitemapDownload.site}")
+    logger.info(Map(
+      "site" -> sitemapDownload.site.url,
+      "site.sitemap.uris.all" -> sitemapDownload.allUris.size,
+      "site.sitemap.uris.old" -> alreadyKnownUris.size,
+      "site.sitemap.uris.new" -> urisNotSeenBefore.size
+    ), s"Storing ${urisNotSeenBefore.size} new uris for ${sitemapDownload.site.url}")
+    println("I would expect to see that logging new uris")
     if (urisNotSeenBefore.isEmpty) Future.successful(()) else {
-      logger.info(Map(
-        "site" -> sitemapDownload.site.url,
-        "site.sitemap.uris.all" -> sitemapDownload.allUris.size,
-        "site.sitemap.uris.old" -> alreadyKnownUris.size,
-        "site.sitemap.uris.new" -> urisNotSeenBefore.size
-      ), s"Storing ${urisNotSeenBefore.size} new uris for ${sitemapDownload.site.url}")
       scanamoAsync.exec(
         table.putAll(urisNotSeenBefore.map(uri => AvailabilityRecord(uri, sitemapDownload.timestamp)))
       )
